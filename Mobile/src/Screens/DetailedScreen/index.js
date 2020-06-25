dcbc8696d8c83a708d67ee26edd871871aae04d3
Kauã Steliano <stelianok@gmail.com>
2020-06-25 09:34:12 -0300

    Added and ajusted img size, added title.

--- Mobile/src/Screens/DetailedScreen/index.js
+++ Mobile/src/Screens/DetailedScreen/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, Image, useWindowDimensions} from 'react-native';
 
 import styles from './styles';
 
-export default function DetailedScreen(){
-    return(
-        <View style={styles.container}>
-            <Text>
-               DetailedScreen
-            </Text>
+export default function DetailedScreen() {
+  const width = useWindowDimensions().width;
+  return (
+    <View style={styles.container}>
+      <View style={[styles.card, {width: width - 55}]}>
+        <View style={styles.imgView}>
+          <Image
+            style={styles.img}
+            source={require('../../../img/placeholder.jpg')}
+          />
         </View>
-    )
+
+        <Text style={styles.title}> Titulo </Text>
+      </View>
+    </View>
+  );
 }
--- Mobile/src/Screens/DetailedScreen/styles.js
+++ Mobile/src/Screens/DetailedScreen/styles.js
@@ -1,9 +1,32 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ff6b99',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: '#1D1D1D',
+
+    marginVertical: 50,
+
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  img: {
+    width: 355,
+    height: 200,
+    resizeMode: 'cover',
+
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  title: {
+    fontSize: 24,
+    color: '#D9D9D9',
+  },
+});
 export default styles;
