b08c7051ee6ede2af41d2c78759bec7f84dad9b5
Gabriel Ishibashi <gabriel-ishibashi@hotmail.com>
2020-07-01 22:17:44 -0300

    Branch errada

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
-
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor="#B1FF92" barStyle="dark-content" />
-      
+     
     </>
   );
 }
